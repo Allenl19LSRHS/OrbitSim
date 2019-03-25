079ad6e04ff8c4d55028952ff4d14dff9e4143e1
Allenl19LSRHS <47216743+Allenl19LSRHS@users.noreply.github.com>
2019-03-25 08:08:48 -0400

    Initial commit

---
+++ display/OrbitSim.java
@@ -0,0 +1,17 @@
+package orbitsim.display;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class OrbitSim extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	
+	public void start(Stage stage) {
+		
+	}
+
+}
