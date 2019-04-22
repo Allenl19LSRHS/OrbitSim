77d1349fa3fae9c9727ae6bc8627e370a5d3e402
Allenl19LSRHS <47216743+Allenl19LSRHS@users.noreply.github.com>
2019-04-22 11:58:03 -0400

    added more comments

--- engine/Universe.java
+++ engine/Universe.java
@@ -31,6 +31,8 @@ public class Universe {
 	
 	public void cycle() {
 		for (Body i : bodies) {
+			// Draw line for previous step
+			//TODO: Maybe animate it too so it draws along with the body moving animation?
 			Line l = new Line(i.getOldX(), i.getOldY(), i.getX(), i.getY());
 			l.setStroke(Color.WHITESMOKE);
 			main.getCanvas().getChildren().add(l);
@@ -52,12 +54,16 @@ public class Universe {
 				finalVec = Vector.addVec(finalVec, b);
 			}
 			
+			// Set the velocity by adding components of previous vel with vel from force vector
 			i.setVelX(i.getVelX() + finalVec.getX());
 			i.setVelY(i.getVelY() + finalVec.getY());
 			
+			// Set the x position based on the new velocity
+			// NOTE: multiplied by .5 to slow it down, maybe make this adjustable?
 			i.setX(i.getX() + (int)(0.5*i.getVelX() * (1000/OrbitSim.timeScale)));
 			i.setY(i.getY() + (int)(0.5*i.getVelY() * (1000/OrbitSim.timeScale)));
 			
+			// Add keyframes based on old and new positions
 			timelineManager.getTimeline().getKeyFrames().addAll(
 					new KeyFrame(Duration.ZERO, new KeyValue(i.getCircle().centerXProperty(), i.getOldX()), new KeyValue(i.getCircle().centerYProperty(), i.getOldY())),
 					new KeyFrame(Duration.millis(OrbitSim.timeScale), timelineManager, new KeyValue(i.getCircle().centerXProperty(), i.getX()), new KeyValue(i.getCircle().centerYProperty(), i.getY()))
